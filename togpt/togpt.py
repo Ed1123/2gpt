7370bf3c32098d9a0d400d5312ef6dd53a0bf0c3
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-03-28 12:03:48 -0500

    Ignore . folders and content of .gitignore
    Add gitignore_parser
    Ignore tmp.md
    Move code to a class to read .gitignore only once

--- togpt/togpt.py
+++ togpt/togpt.py
@@ -1,47 +1,51 @@
 from pathlib import Path
 
-
-def build_tree(directory: Path, prefix: str = '') -> str:
-    '''
-    Builds a tree structure of the given directory.
-    '''
-    output = ''
-    path = Path(directory)
-
-    # Iterate over all files and subdirectories in the directory
-    for item in sorted(path.iterdir()):
-        if item.is_dir():
-            # If the item is a directory, recursively build a tree for it
-            output += f'{prefix}└── {item.name}/\n'
-            output += build_tree(item, prefix + ' ' * 4)
-        else:
-            # If the item is a file, add it to the output
-            output += f'{prefix}└── {item.name}\n'
-
-    return output
+from gitignore_parser import parse_gitignore
 
 
-def get_file_contents(directory: Path) -> str:
+class TextBuilder:
+    def __init__(self, directory: Path) -> None:
+        self.directory = directory
+        self.gitignore = parse_gitignore(directory / '.gitignore')
+
+    def build_text(self, directory: Path, prefix: str = '') -> tuple[str, str]:
+        '''
+        Builds a tree structure of the given directory.
+        '''
+        tree = ''
+        files = ''
+
+        # Iterate over all files and subdirectories in the directory
+        for item in sorted(directory.iterdir()):
+            if item.name.startswith('.') or self.gitignore(item):
+                continue
+            if item.is_dir():
+                # If the item is a directory, recursively build a tree for it
+                tree += f'{prefix}└── {item.name}/\n'
+                tree_, files_ = self.build_text(item, prefix + ' ' * 4)
+                tree += tree_
+                files += files_
+            else:
+                # If the item is a file, add it to the output
+                tree += f'{prefix}└── {item.name}\n'
+                files += get_file_contents(item)
+
+        return tree, files
+
+
+def get_file_contents(path: Path) -> str:
     '''
     Returns the contents of all Python files in the given directory.
     '''
-    output = ''
-
-    # Iterate over all files in the directory
-    for item in sorted(directory.iterdir()):
-        if item.is_file() and item.suffix == '.py':
-            # If the item is a Python file, add its contents to the output
-            with item.open('r') as file:
-                output += file.read() + '\n\n'
-
-    return output
+    with path.open('r') as file:
+        header = f'# Contents of {path.name}'
+        return header + '\n```\n' + file.read() + '\n```\n\n'
 
 
 def convert_project_folder(directory: Path) -> str:
     '''
     Converts a project folder into a format digestible by ChatGPT.
     '''
-    tree = build_tree(directory, 'asfasfd')
-    contents = get_file_contents(directory)
-
-    return f'{tree}\n\n{contents}'
+    text_builder = TextBuilder(directory)
+    text = text_builder.build_text(directory)
+    return f'# Project tree structure\n```\n{text[0]}```\n\n{text[1]}\n'
